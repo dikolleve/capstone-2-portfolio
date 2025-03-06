c17eac35052b55d6790a4e1ee1075a7207731052
dikolleve <diko.lleve09@gmail.com>
2025-03-06 06:13:59 -0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,97 @@
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText", {
+  strings: ["Designer", "Programmer", "Developer"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000,
+});
+
+/* ----- HAMBURGER MENU ----- */
+const navbar = document.querySelector(".navbar");
+const openSideBar = document.querySelector("#open-sidebar i");
+const closeSidebar = document.querySelector("#close-sidebar i");
+const overlay = document.querySelector("#overlay");
+
+const showSideBar = () => {
+  navbar.classList.add("show");
+};
+
+const dontShowSideBar = () => {
+  navbar.classList.remove("show");
+};
+
+openSideBar.addEventListener("click", showSideBar);
+closeSidebar.addEventListener("click", dontShowSideBar);
+overlay.addEventListener("click", dontShowSideBar);
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".navbar a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".navbar a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+/* -- HOME -- */
+sr.reveal(".featured-text", {});
+sr.reveal(".featured-name", { delay: 100 });
+sr.reveal(".featured-text-info", { delay: 200 });
+sr.reveal(".featured-text-btn", { delay: 200 });
+sr.reveal(".social_icons", { delay: 200 });
+sr.reveal(".featured-image", { delay: 300 });
+
+/* -- PROJECT BOX -- */
+/* interval to make successive effect */
+sr.reveal(".project-box", { interval: 200 });
+
+/* -- HEADINGS -- */
+sr.reveal(".top-header", {});
+
+/* --ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: "left",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+srLeft.reveal(".about-info", { delay: 100 });
+srLeft.reveal(".col-contact", { delay: 100 });
+
+/* -- ABOUT SKILLS & FORM BOX-- */
+const srRight = ScrollReveal({
+  origin: "right",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+srRight.reveal(".skills-box", { delay: 100 });
+srRight.reveal(".form-control", { delay: 100 });
